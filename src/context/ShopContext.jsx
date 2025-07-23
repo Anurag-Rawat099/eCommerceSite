b646fd107ed1f53d8e8fa600b7f6f66c7f0de053
Your Name <anuraw@099gmail.com>
2025-07-23 09:20:40 +0530

    modified shopcontext

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,14 +1,37 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets.js";
 export const ShopContext = createContext();
 
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const deliveryFee = 10;
-  const value = {
+  const [cartItem, setCartItem] = useState({});
+  const addToCart = (itemId, size) => {
+    if (!size) {
+      alert("select the size");
+      return;
+    }
+    let cartData = structuredClone(cartItem);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    }else{
+      cartData[itemId]={}
+      cartData[itemId][size]=1;
+    }
+
+  };
+useEffect(()=>{
+  console.log(cartItem);
+})
+const value = {
     products,
     currency,
     deliveryFee,
+    addToCart,
   };
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
